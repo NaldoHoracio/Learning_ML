e57cc4716f6d374e6ce87d9fa4e28e9646cb3c5f
NaldoHorácio <edvonaldohs@ic.ufal.br>
2020-09-14 20:24:38 -0300

    Files version.

--- versionamento.py
+++ versionamento.py
@@ -7,55 +7,49 @@ Created on Thu Sep 10 13:03:14 2020
 
 #!/usr/bin/env python
 import re
+import os
 import csv
 import pandas as pd
-import os.path
+#import os.path
 
-fields_version = ['Version']
+name_file = os.path.relpath("../Learning_ML/Logs/version.csv")
 
-rows_version = []
+def version_file(name_file):
+    #fields_version = ['Version', 'Teste']
+    
+    rows_version = []
+    
+    if os.path.isfile(name_file):
+        file_version_py = name_file      
+        df = pd.read_csv(name_file, delimiter=';')
+        teste = df['Version'].iloc[-1]
+        value = int(teste)
+        value_ = int(teste)
+        value += 1
+        value_ += 2
+        rows_version = [[value, value_]]
+        with open(file_version_py, 'a') as csvfile:
+            # creating a csv writer object  
+            csvwriter = csv.writer(csvfile, delimiter=';') 
+            # writing the fields  
+            #csvwriter.writerow(field_version)    
+            # writing the data rows  
+            csvwriter.writerows(rows_version) 
+    else:
+        teste = int(0)
+        teste = 1
+        file_version_py = name_file
+        fields_version = ['Version', 'Teste']
+        rows_version = [[teste, teste]]
+        with open(file_version_py, 'a') as csvfile:
+            # creating a csv writer object  
+            csvwriter = csv.writer(csvfile, delimiter=';')
+            # writing the fields  
+            csvwriter.writerow(fields_version)  
+            # writing the data rows  
+            csvwriter.writerows(rows_version) 
+            print ("File not exist")
 
-
-if os.path.isfile('Logs/version.csv'):
-    file_version_py = "Logs/version.csv"
-    
-    df = pd.read_csv(r'Logs/version.csv')
-    
-    teste = df['Version'].iloc[-1]
-    
-    value = int(teste)
-    
-    value += 1
-    
-    #field_version = ['Version']
-
-    rows_version = [[value]]
-    
-    with open(file_version_py, 'a') as csvfile:
-        # creating a csv writer object  
-        csvwriter = csv.writer(csvfile, delimiter=';') 
-        # writing the fields  
-        #csvwriter.writerow(field_version)    
-        # writing the data rows  
-        csvwriter.writerows(rows_version) 
-else:
-    teste = int(0)
-
-    teste = 1
-
-    file_version_py = "Logs/version.csv"
-    
-    field_version = ['Version']
-    
-    rows_version = [[teste]]
-    with open(file_version_py, 'a') as csvfile:
-        # creating a csv writer object  
-        csvwriter = csv.writer(csvfile, delimiter=';')
-        # writing the fields  
-        csvwriter.writerow(fields_version)  
-        # writing the data rows  
-        csvwriter.writerows(rows_version) 
-    print ("File not exist")
-
-        
+#%% Return file
+version_file(name_file)
      
