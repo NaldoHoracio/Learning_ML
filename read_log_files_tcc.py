835e80f7241659f0f4901a8a69d8395c69b08da6
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2020-09-17 15:04:44 -0300

    Create read_log_files_tcc.py

---
+++ read_log_files_tcc.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Título: Lendo os arquivos de Log em csv
+
+@author: edvonaldo
+"""
+
+import os
+import csv
+import math
+import random
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
