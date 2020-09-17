39b780c05f3a4076fde169a1373e75e85f57ce23
edvonaldohoracio <edvonaldohs@ic.ufal.br>
2020-09-17 17:14:36 -0300

    Update read_log_files_tcc.py

--- read_log_files_tcc.py
+++ read_log_files_tcc.py
@@ -13,3 +13,16 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
+
+
+metrics_al_dt = pd.read_csv(r'compare_methods/Logs/METRICS_EVALUATE/DT_AL.csv')
+
+metrics_al_rf = pd.read_csv(r'compare_methods/Logs/METRICS_EVALUATE/RF_AL.csv')
+
+metrics_al_ls = pd.read_csv(r'compare_methods/Logs/METRICS_EVALUATE/LS_AL.csv')
+
+metrics_br_dt = pd.read_csv(r'compare_methods/Logs/METRICS_EVALUATE/DT_BR.csv')
+
+metrics_br_rf = pd.read_csv(r'compare_methods/Logs/METRICS_EVALUATE/RF_BR.csv')
+
+metrics_br_ls = pd.read_csv(r'compare_methods/Logs/METRICS_EVALUATE/LS_BR.csv')
